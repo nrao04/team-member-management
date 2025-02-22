e2fe9e069c54f1bfccfd6595dd9853f889809b07
nrao <nrao200423@gmail.com>
2025-02-22 02:01:51 -0800

    refactored team member model: improved field names & added help_text for guidance

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework', #django REST API
+    'team',
 ]
 
 MIDDLEWARE = [
---
+++ team/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# rep person on team with personal details
+class teamMember(models.Model):
+    # diff roles, first val stored in database, second val shown in forms
+    role_options = [('admin', 'Admin'), ('reg', 'Regular')]
+    # team member contact info (name, email, phone, role)
+    first_name = models.CharField(max_length = 35, help_text = "Enter member's first name")
+    last_name = models.CharField(max_length = 35, help_text = "Enter member's last name")
+    email_addr = models.EmailField(unique = True, help_text = "Provide a valid email address")
+    phone_num = models.CharField(max_length = 16, help_text = "Enter a phone number in format: (ex: 123-456-7890)")
+    role = models.CharField(max_length = 16, options = role_options, default = 'reg', help_text = "Select member's role (Admin or Regular Member)")
+    
