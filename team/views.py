f48048a9d7cd7bee35f1fdd41e79dca319c087f2
nrao <nrao200423@gmail.com>
2025-02-22 18:03:24 -0800

    added API view for listing & creating team members

--- team/views.py
+++ team/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import teamMember
+from .serializers import tmSerializer
 
-# Create your views here.
+# API view for managing entire team roster (list & create)
+class tmDirectoryView(generics.rosterManagementAPI):
+    # get all team members
+    team_members_data = teamMember.objects.all()
+    # conv. data to JSON for API
+    team_serializer = tmSerializer
