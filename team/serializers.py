2b44a824b2b7cf799880e50bef9cab676a9d7a9d
nrao <nrao200423@gmail.com>
2025-02-22 16:20:18 -0800

    implemented member serializer with limited fields for improved security

---
+++ team/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import teamMember
+
+# automates model for JSON conv.(APIs)
+class tmSerializer (serializers.modelSerializer):
+    model = teamMember
+    # incl. only some fields for better secruity, more control
+    fields = ['first_name', 'last_name', 'email_addr']
