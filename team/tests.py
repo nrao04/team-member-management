913b959c34d85c1d7a2caab1ab56d44b203c1894
nrao <nrao200423@gmail.com>
2025-02-22 21:22:34 -0800

    fixed and ensured tests for team member creation works appropriately

--- team/tests.py
+++ team/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import teamMember
 
-# Create your tests here.
+# testing if team members created appropriately
+class tmTestCases(TestCase):
+    # create team member before test (using setUp for faster debugging)
+    def setUp(self):
+        teamMember.objects.create(first_name = "Nikhil", last_name = "Rao", role = "admin")
+        
+    def test_addTeamMember(self):
+        nikhil = teamMember.objects.get(first_name = "Nikhil")
+        # use assert equal to check if role is correct
+        self.assertEqual(nikhil.last_name, "Rao")
+        self.assertEqual(nikhil.role, "admin")
