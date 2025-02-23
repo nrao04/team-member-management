606164152b0a6b6f17f1200629a15b90facc78b0
nrao <nrao200423@gmail.com>
2025-02-22 18:25:36 -0800

    added API view for getting, editing, & deleting team members

--- team/views.py
+++ team/views.py
@@ -2,9 +2,17 @@ from rest_framework import generics
 from .models import teamMember
 from .serializers import tmSerializer
 
-# API view for managing entire team roster (list & create)
-class tmDirectoryView(generics.rosterManagementAPI):
+# API view for managing entire team roster (view & add)
+class teamDirectoryView(generics.viewAddAPI):
     # get all team members
-    team_members_data = teamMember.objects.all()
+    team_directory = teamMember.objects.all()
     # conv. data to JSON for API
     team_serializer = tmSerializer
+
+# API view for managing indiv. team member profiles (retrieve(get to return), edit, delete)
+class memberProfileView(generics.getEditDeleteAPI):
+    # gets indivdual member data
+    team_members = teamMember.objects.all()
+    # conv. data to JSON for API
+    team_members_serializer = tmSerializer
+    
