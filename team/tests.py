eca727a5dfd9d637c23e60caa5af936e4606862f
nrao <nrao200423@gmail.com>
2025-02-23 18:39:02 -0800

    finalized backend for now

--- team/tests.py
+++ team/tests.py
@@ -6,7 +6,8 @@ class tmTestCases(TestCase):
     # create team member before test (using setUp for faster debugging)
     def setUp(self):
         teamMember.objects.create(first_name = "Nikhil", last_name = "Rao", role = "admin")
-        
+    
+    # testing myself as team member    
     def test_addTeamMember(self):
         nikhil = teamMember.objects.get(first_name = "Nikhil")
         # use assert equal to check if role is correct
