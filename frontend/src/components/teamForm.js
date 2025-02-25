8ed59da7e9df069f29bcf36f547d97a20631d3f6
nrao <nrao200423@gmail.com>
2025-02-25 02:40:59 -0800

    finished teamForm component for adding or editing members

--- frontend/src/components/teamForm.js
+++ frontend/src/components/teamForm.js
@@ -11,6 +11,29 @@ const teamForm = ({onSubmit, initialData = {first_name: '', last_name: '', role:
         setFormData({...formData, [e.target.name]: e.target.value})
     };
 
-    //
+    // handles form submission
+    const formSubmit = (e) => {
+        // prevents page reload
+        e.preventDefault(); 
+        // calls par. funct. with form data
+        onSubmit(formData)
+    };
 
+    return (
+        <form onSubmit={formSubmit}>
+            <label>First Name:</label>
+            <input type = "text" name = "first_name" value = {formData.first_name} onChange = {formFieldChange} required />
+
+            <label>Last Name:</label>
+            <input type = "text" name = "last_name" value = {formData.last_name} onChange = {formFieldChange} required />
+
+            <label>Role:</label>
+            <select name = "role" value = {formData.role} onChange = {formFieldChange}>
+                <option value = "regular">Regular</option>
+                <option value = "admin">Admin</option>
+            </select>
+
+            <button type = "submit" >Submit</button>
+        </form>
+    );
 };
