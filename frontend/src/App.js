e70aa820599f8173e071670bc476f3ab9b488c65
nrao <nrao200423@gmail.com>
2025-03-06 01:02:32 -0800

    fixed syntax error on App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,13 +15,13 @@ function App() {
                 {/* defines diff routes for the app */}
                 <Routes>
                     {/* home page: displays the list of team members */}
-                    <Route path="/" element={<listPage />} />
+                    <Route path="/" element={<ListPage />} />
 
                     {/* page for adding a new team member */}
-                    <Route path="/add" element={<addPage />} />
+                    <Route path="/add" element={<AddPage />} />
 
                     {/* page for editing an existing team member */}
-                    <Route path="/edit/:id" element={<editPage />} />
+                    <Route path="/edit/:id" element={<EditPage />} />
                 </Routes>
             </div>
         </Router>
