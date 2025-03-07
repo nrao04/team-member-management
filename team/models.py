8e15a0dc2ffc7daa0c03aa7aacf8add544068d10
nrao <nrao200423@gmail.com>
2025-03-06 19:21:56 -0800

    fixed issue with admin selection

--- team/models.py
+++ team/models.py
@@ -11,6 +11,13 @@ class teamMember(models.Model):
     phone_number = models.CharField(max_length = 16, blank = False, null = False, help_text = "Enter a phone number in format: (ex: 123-456-7890)")
     role = models.CharField(max_length = 16, choices = role_options, default = 'regular', blank = False, null = False, help_text = "Select member's role (Admin or Regular Member)")
     
+     # override the save method to ensure only one admin exists at a time
+    def save(self, *args, **kwargs):
+        if self.role == 'admin':
+            # set all other members to 'regular' before saving
+            teamMember.objects.filter(role='admin').update(role='regular')
+        super().save(*args, **kwargs)
+    
     # returns more readable representation of member
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.role})"
